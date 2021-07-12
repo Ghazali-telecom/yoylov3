abad6e1962c4491c230e29db6537570a54be997e
ghazali med <ghazali.yfm@gmail.com>
2021-07-12 18:28:29 +0200

    ADD setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yolov3_gh',
+    version='1.0.0',
+    url='https://github.com/Ghazali-telecom/yoylov3.git',
+    author='ghazali',
+    author_email='ghazali.yfm@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires='requirements.txt'
+)
