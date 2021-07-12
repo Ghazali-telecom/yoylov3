3b7ddfceac0b9b2c40f883e320b7ab374faf9a1f
ghazali med <ghazali.yfm@gmail.com>
2021-07-12 18:50:45 +0200

    FIX requirment version

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='ghazali.yfm@gmail.com',
     description='Description of my package',
     packages=find_packages(),    
-    install_requires=['numpy >=1.21.0','torch>=1.0','torchvision=0.10.0','matplotlib >=3.4.2','tensorflow==1.15','tensorboard>=2.5.0','terminaltables>=3.1.0'
+    install_requires=['numpy >=1.21.0','torch>=1.0','torchvision==0.10.0','matplotlib >=3.4.2','tensorflow==1.15','tensorboard>=2.5.0','terminaltables>=3.1.0'
     ,'pillow>=8.3.1','tqdm>=4.61.2'],
 )
